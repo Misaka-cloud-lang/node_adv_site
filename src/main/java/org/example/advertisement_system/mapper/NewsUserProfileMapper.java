4713d6c3f204557056cf2eeabe9e1036dbd0160a
weirenran <1392465152@qq.com>
2024-12-23 14:54:34 +0800

    （郑岳平）调整新闻用户画像的dao层操作，新闻的用户标签权重只需要+1，没有+2和+3的操作

--- src/main/java/org/example/advertisement_system/mapper/NewsUserProfileMapper.java
+++ src/main/java/org/example/advertisement_system/mapper/NewsUserProfileMapper.java
@@ -18,23 +18,6 @@ public interface NewsUserProfileMapper {
      * @param userProfile 用户标签及其权重
      */
     void insertOrUpdateUserProfile(UserProfile userProfile);
-
-    /**
-     * 插入或更新用户标签及其权重。
-     * 如果标签已存在，则权重加2；否则插入新标签并设置权重为2。
-     *
-     * @param userProfile 用户标签及其权重
-     */
-    void insertOrUpdateUserProfileWithWeight2(UserProfile userProfile);
-
-    /**
-     * 插入或更新用户标签及其权重。
-     * 如果标签已存在，则权重加3；否则插入新标签并设置权重为3。
-     *
-     * @param userProfile 用户标签及其权重
-     */
-    void insertOrUpdateUserProfileWithWeight3(UserProfile userProfile);
-
     /**
      * 根据用户ID获取所有用户标签及其权重。
      *
