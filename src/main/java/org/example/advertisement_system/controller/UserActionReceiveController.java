04af2f3a5c05a1b40f386a397f540ba8fd5fd73b
goldfish9901 <745727212@qq.com>
2024-12-23 14:08:31 +0800

    （金予乐） 接受电商和新闻网站用户行为

--- src/main/java/org/example/advertisement_system/controller/UserActionReceiveController.java
+++ src/main/java/org/example/advertisement_system/controller/UserActionReceiveController.java
@@ -3,7 +3,10 @@ package org.example.advertisement_system.controller;
 import org.example.advertisement_system.entity.UserProfile;
 import org.example.advertisement_system.mapper.EMallUserActionReceiveMapper;
 import org.example.advertisement_system.mapper.NewsSiteUserActionMapper;
+import org.example.advertisement_system.mapper.NewsUserProfileMapper;
+import org.example.advertisement_system.mapper.StoreUserProfileMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +21,12 @@ public class UserActionReceiveController {
     @Autowired
     private EMallUserActionReceiveMapper eMallUserActionReceiveMapper;
 
+    @Autowired
+    private NewsUserProfileMapper newsUserProfileMapper;
+
+    @Autowired
+    private StoreUserProfileMapper storeUserProfileMapper;
+
     /**
      * 接收用户的新闻网站行为
      * @param userId 用户id（转发cookie）
@@ -26,7 +35,7 @@ public class UserActionReceiveController {
      */
 
     @PostMapping("/news")
-    public void receiveNewsSiteAction(
+    public ResponseEntity<UserProfile> receiveNewsSiteAction(
             @RequestParam(name="userId",defaultValue = "-1")
             int userId,
             @RequestParam(name="tag",defaultValue = "Unknown")
@@ -38,13 +47,21 @@ public class UserActionReceiveController {
         profile.setUserId(userId);
         profile.setTagName(tag);
         profile.setTagWeight(tag_value);
-        newsSiteUserActionMapper.insertUserAction(profile);
+//        newsSiteUserActionMapper.insertUserAction(profile);
+        switch ((int)tag_value){
+            case 1:
+                newsUserProfileMapper.insertOrUpdateUserProfile(profile);
+                break;
+            default:
+                return ResponseEntity.badRequest().body(profile);
+        }
         System.out.println("receive news_site action");
         System.out.println(profile);
+        return ResponseEntity.ok(profile);
     }
 
     @PostMapping("/emall")
-    public void receiveEMallSiteAction(
+    public ResponseEntity<UserProfile> receiveEMallSiteAction(
             @RequestParam(name="userId",defaultValue = "-1")
             int userId,
             @RequestParam(name="tag",defaultValue = "Unknown")
@@ -56,9 +73,24 @@ public class UserActionReceiveController {
         profile.setUserId(userId);
         profile.setTagName(tag);
         profile.setTagWeight(tag_value);
-        eMallUserActionReceiveMapper.insertEMallUserAction(profile);
-        System.out.println("receive emall action");
+//        eMallUserActionReceiveMapper.insertEMallUserAction(profile);
+        switch ((int)tag_value){
+            case 1:
+                newsUserProfileMapper.insertOrUpdateUserProfile(profile);
+                break;
+            case 2:
+                storeUserProfileMapper.insertOrUpdateStoreUserProfileWithWeight2(profile);
+                break;
+            case 3:
+                storeUserProfileMapper.insertOrUpdateStoreUserProfileWithWeight3(profile);
+                break;
+            default:
+                return ResponseEntity.badRequest().body(profile);
+
+        }
+        System.out.println("receive store action");
         System.out.println(profile);
+        return ResponseEntity.ok(profile);
 
     }
 }
