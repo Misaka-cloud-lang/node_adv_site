d147a23b3c32438d8ae1da934686977af8703003
goldfish9901 <745727212@qq.com>
2024-12-29 22:47:47 +0800

    （金予乐） 指定服务器端日志文件位置

--- src/main/java/org/example/advertisement_system/controller/UserActionReceiveController.java
+++ src/main/java/org/example/advertisement_system/controller/UserActionReceiveController.java
@@ -5,10 +5,7 @@ import org.example.advertisement_system.mapper.NewsUserProfileMapper;
 import org.example.advertisement_system.mapper.StoreUserProfileMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/receive")
