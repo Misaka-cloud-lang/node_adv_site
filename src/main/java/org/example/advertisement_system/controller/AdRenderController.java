bf84e319df0c5f7be82da6d7e1922b963deec4bd
goldfish9901 <745727212@qq.com>
2024-12-31 00:11:20 +0800

    （金予乐） 增加电商投放mapping

--- src/main/java/org/example/advertisement_system/controller/AdRenderController.java
+++ src/main/java/org/example/advertisement_system/controller/AdRenderController.java
@@ -4,10 +4,12 @@ import org.example.advertisement_system.entity.Advertisement;
 import org.example.advertisement_system.entity.UserProfile;
 import org.example.advertisement_system.service.AdPlacementService;
 import org.example.advertisement_system.service.NewsUserProfileService;
+import org.example.advertisement_system.service.StoreUserProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
@@ -17,21 +19,36 @@ import java.util.List;
  * @author jyl
  */
 @Controller
-//@RequestMapping("/api/ad")
+@RequestMapping("/api/ad")
 public class AdRenderController {
     @Autowired
     private AdPlacementService adPlacementService;
     @Autowired
     private NewsUserProfileService newsUserProfileService;
+    @Autowired
+    private StoreUserProfileService storeUserProfileService;
 
-    @GetMapping("/api/ad/news")
+    @GetMapping("/news")
     public String renderNewsAd(
             @RequestParam(name = "userId", defaultValue = "1") int userId,
+            @RequestParam(name="adLocation", defaultValue = "0") int adLocation,
             Model model
     ) {
         List<UserProfile> profile = newsUserProfileService.getUserProfilesByUserId(userId);
         List<Advertisement> newsAds = adPlacementService.getNewsAdPlacements(userId, profile);
-        Advertisement advertisement = newsAds.get(0);
+        Advertisement advertisement = newsAds.get(adLocation%newsAds.size());
+        model.addAttribute("ad", advertisement);
+        return "adIframe";
+    }
+    @GetMapping("/store")
+    public String renderStoresAd(
+            @RequestParam(name = "userId", defaultValue = "1") int userId,
+            @RequestParam(name="adLocation", defaultValue = "0") int adLocation,
+            Model model
+    ) {
+        List<UserProfile> profile = storeUserProfileService.getStoreUserProfilesByUserId(userId);
+        List<Advertisement> newsAds = adPlacementService.getNewsAdPlacements(userId, profile);
+        Advertisement advertisement = newsAds.get(adLocation%newsAds.size());
         model.addAttribute("ad", advertisement);
         return "adIframe";
     }
